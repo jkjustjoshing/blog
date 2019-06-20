b5dc143ed5f047a40fca61acfdb3ecef89a9d1d9
Josh Kramer <josh.kramer@measuresforjustice.org>
2019-06-19 22:34:49 -0400

    Create index page matching kramer.run

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from '../components/Layout'
+
+import styles from './index.module.scss'
+
+const IndexPage = () => {
+  return (
+    <Layout mainClassName={styles.main}>
+      blog, test
+    </Layout>
+  )
+}
+
+export default IndexPage
